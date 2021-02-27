4a24e7be24617b683fbace3a478ff460c72b5475
Joshua Robison <tuf57986@temple.edu>
2021-02-27 17:55:58 -0500

    Add files via upload

---
+++ src/data-structures/binary-search-tree.js
@@ -0,0 +1,103 @@
+// Data Structures in JavaScript: Binary Search Tree
+function BinarySearchTree(data) {
+  this.data = data;
+  this.left = null;
+  this.right = null;
+}
+
+// insert a node (sub-tree) into the binary search tree
+BinarySearchTree.prototype.insert = function(data) {
+  if (data < this.data) {
+    if (!this.left) {
+      this.left = new BinarySearchTree(data);
+    } else {
+      this.left.insert(data);
+    }
+  } else if (data > this.data) {
+    if (!this.right) {
+      this.right = new BinarySearchTree(data);
+    } else {
+      this.right.insert(data);
+    }
+  } else {
+    return null;
+  }
+};
+
+// check if a value exists in the binary search tree
+BinarySearchTree.prototype.contains = function(data) {
+  if (data === this.data) {
+    return true;
+  } else if (data < this.data) {
+    if (!this.left) {
+      return false;
+    } else {
+      return this.left.contains(data);
+    }
+  } else if (data > this.data) {
+    if (!this.right) {
+      return false;
+    } else {
+      return this.right.contains(data);
+    }
+  } else {
+    return false;
+  }
+};
+
+// depth first search: pre-order, in-order, post-order
+BinarySearchTree.prototype.dfs = function(order = "in-order", iterator = log) {
+  if (order === "pre-order") {
+    iterator(this.data);
+  }
+  if (this.left) {
+    this.left.dfs(order, iterator);
+  }
+  if (order === "in-order") {
+    iterator(this.data);
+  }
+  if (this.right) {
+    this.right.dfs(order, iterator);
+  }
+  if (order === "post-order") {
+    iterator(this.data);
+  }
+};
+
+// breadth first search
+BinarySearchTree.prototype.bfs = function(iterator = log) {
+  var queue = [this];
+  while (queue.length) {
+    var node = queue.shift();
+    iterator(node.data);
+    if (node.left) {
+      queue.push(node.left);
+    }
+    if (node.right) {
+      queue.push(node.right);
+    }
+  }
+};
+
+// minimum value from bst
+BinarySearchTree.prototype.minimum = function() {
+  if (this.left) {
+    return this.left.minimum();
+  } else {
+    return this.data;
+  }
+};
+
+// maximum value from bst
+BinarySearchTree.prototype.maximum = function() {
+  if (this.right) {
+    return this.right.maximum();
+  } else {
+    return this.data;
+  }
+};
+
+// iterator function
+function log(data) {
+  console.log(data);
+}
---
+++ src/data-structures/linked-list.js
@@ -0,0 +1,97 @@
+// Data Structures in JavaScript: Linked List
+function LinkedList() {
+  this.head = null;
+  this.tail = null;
+  this.length = 0;
+}
+
+// a node in the linked list
+function Node(data, next, prev) {
+  this.data = data;
+  this.next = next;
+  this.prev = prev;
+}
+
+// add new node to head of linked list
+LinkedList.prototype.addToHead = function(data) {
+  let node = new Node(data, this.head, null);
+  if (this.head) {
+    this.head.prev = node;
+  } else {
+    this.tail = node;
+  }
+  this.head = node;
+  this.length++;
+};
+
+// add new node to tail of linked list
+LinkedList.prototype.addToTail = function(data) {
+  let node = new Node(data, null, this.tail);
+  if (this.tail) {
+    this.tail.next = node;
+  } else {
+    this.head = node;
+  }
+  this.tail = node;
+  this.length++;
+};
+
+// remove head from linked list
+LinkedList.prototype.removeHead = function() {
+  if (!this.head) {
+    return null;
+  }
+  var data = this.head.data;
+  this.head = this.head.next;
+  if (this.head) {
+    this.head.prev = null;
+  } else {
+    this.tail = null;
+  }
+  this.length--;
+  return data;
+};
+
+// remove tail from linked list
+LinkedList.prototype.removeTail = function() {
+  if (!this.tail) {
+    return null;
+  }
+  var data = this.tail.data;
+  this.tail = this.tail.prev;
+  if (this.tail) {
+    this.tail.next = null;
+  } else {
+    this.head = null;
+  }
+  this.length--;
+  return data;
+};
+
+// search linked list for data
+LinkedList.prototype.search = function(data) {
+  var node = this.head;
+  while (node) {
+    if (node.data == data) {
+      return node;
+    } else {
+      node = node.next;
+    }
+  }
+  return null;
+};
+
+// get indices of linked list data
+LinkedList.prototype.indexOf = function(data) {
+  var indices = new Array();
+  var index = 0;
+  var node = this.head;
+  while (node) {
+    if (node.data == data) {
+      indices.push(index);
+    }
+    node = node.next;
+    index++;
+  }
+  return indices;
+};
